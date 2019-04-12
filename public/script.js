2fcf6edfb7c71a690e435bdbeea555870d5d7cf6
Priyansha Chichra <priyansha9998@gmail.com>
2019-04-12 10:07:50 +0530

    join rooms

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
     const socket = io()
 
+    //initialize the socket
     socket.on('connect', () => {
         console.log('Socket created ' + socket.id)
     })
@@ -10,40 +11,117 @@ $(() =>{
     let $messageBox = $('#message');
     let $chat = $('#chat');
     let $username = $('#username')
+    let $room = $('#room')
+    let $roomHead = $('#room-head')
     
-
+    //actions taking place when the message form is submitted
     $messageForm.submit((e) =>{
+        //to make sure that the default actions do not take place
         e.preventDefault();
+        //send the entered username and password  to the server
         socket.emit('send_message', {
             username : $username.val(),
-            message : $messageBox.val() 
+            message : $messageBox.val(),
+            room : $room.val()
         })
-        $messageBox.val('');
+        //clear the message box
+        $messageBox.val('')
+        $username.prop("disabled", "true")
+        
     })
-
+    
     socket.on('new_message', (data) =>{
+        
+        
 
+
+        //when the server send the message to be displayed
         if(data.username == $username.val())
-        {
+        {   
+            if($room.prop("disabled")== false)
+            {
+                $roomHead.append(
+                    $('<h2>').css("text-align", "center").text(
+                    `Room ${data.room}`
+                    )
+                    )
+            $room.prop("disabled", "true")
+            }
+            
+            //if the message is sent by you it goes to the right side of the screen
             $chat.append(
-                $('<h4>').css({
-                    "color":"green",
-                    "float" : "right"
-                })
-                .text(
-                    `${data.message}`
+                $('<div>').addClass("container").append(
+                    $('<div>').addClass("row").append(
+                        $('<div>').addClass("col").append(
+                            $('<h4>').css({
+                            "float" : "right",
+                            "color": "cyan"
+                            })
+                            .text(
+                                `${data.message}`
+                            )
+                        )
+                    )
                 )
             )
-            $chat.append($('<br>'))
         }
         else
         {
+            //if the message is not sent by you it goes to the left side of the screen
             $chat.append(
-                $('<h4>').css("color","red")
-                .text(
-                    `${data.username} : ${data.message}`
-                )
-            )   
-        }  
-    })
-})
+                $('<div>').addClass("container").append(
+                    $('<div>').addClass("row").append(
+                        $('<div>').addClass("col").append(
+                            $('<h4>').css("color","palegreen")
+                                .text(
+                                    `${data.username} : ${data.message}`
+                                )
+                            )
+                        )
+                    )        
+                )   
+            }  
+        }
+    )
+
+    socket.on('updatechat', (data) => {
+        $chat.append(
+            $chat.append(
+                $('<div>').addClass("container").append(
+                    $('<div>').addClass("row").append(
+                        $('<div>').addClass("col").append(
+                            $('<h5>').css({
+                                "background-color" : "white",
+                            })
+                            .text(
+                                    `${data.username} has entered the room`
+                            )
+                            )
+                        )
+                    )        
+                )  
+            ) 
+            }
+        )
+
+    socket.on('updatemychat', (data) => {
+
+        $chat.append(
+            $chat.append(
+                $('<div>').addClass("container").append(
+                    $('<div>').addClass("row").append(
+                        $('<div>').addClass("col").append(
+                            $('<h5>').css({
+                                "background-color" : "white",
+                            })
+                            .text(
+                                    `You have entered room ${data.room}`
+                            )
+                            )
+                        )
+                    )        
+                )  
+            ) 
+            }
+        )    
+	})
--- server.js
+++ server.js
@@ -6,19 +6,59 @@ const socketio = require('socket.io')
 
 const io = socketio(server)
 
+//users that are currently present to chat
+let users = {}
+//rooms currently in use
+let rooms = {}
+
+//Connecting the socket
 io.on('connection', (socket) => {
+//The socket is now connected succesfully
     console.log('Socket connected ' + socket.id)
 
+    socket.on('adduser', (username) => {
+		// store the username in the socket session for this client
+		socket.username = username;
+		// store the room name in the socket session for this client
+		socket.room = 'room1';
+		// add the client's username to the global list
+		usernames[username] = username;
+		// send client to room 1
+		socket.join('room1');
+		// echo to client they've connected
+		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
+		// echo to room 1 that a person has connected to their room
+		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
+		socket.emit('updaterooms', rooms, 'room1');
+	});
+
+
+    //When the user sends a message
     socket.on('send_message', (data) =>{
+        socket.username = data.username
+        socket.room = data.room
+        rooms[data.room] = data.room
+        users[data.username] = data.username
+        socket.join(socket.room)
+        socket.emit('updatemychat', {
+            room : socket.room
+        })
+        socket.broadcast.to(socket.room).emit('updatechat', {
+            username : socket.username
+        });
         //broadcast.emit('new message', data); send to everybody but me
-        io.emit('new_message', {
+        //send the mesage to the clients
+        io.in(socket.room).emit('new_message', {
             username : data.username,
+            room : data.room,
             message : data.message
-        })      
+        });
     })
 })
+//use all the files in the public folder
 app.use('/', express.static(__dirname + '/public'))
 
+//listen the client
 server.listen(3033, () => {
     console.log('Server started on http://localhost:3033')
   })
